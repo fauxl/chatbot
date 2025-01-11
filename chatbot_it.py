a407397af91ee5d2a5d71da8c0282826d7f668fe
Gerardo De Rosa <32306445+fauxl@users.noreply.github.com>
2025-01-11 13:40:15 +0100

    Update chatbot_it.py

--- chatbot_it.py
+++ chatbot_it.py
@@ -62,7 +62,7 @@ def display_chatbot_page():
                 text = st.text_input("Hugging Face Token (No need to insert)", type='password',value= f"{'*' * len(os.getenv('API_KEY'))}")
 
             with row_1[1]:
-                llm_model = st.text_input("LLM model", value="bigscience/bloom")
+                llm_model = st.text_input("LLM model", value="GroNLP/gpt2-small-italian")
 
             with row_1[2]:
                 instruct_embeddings = st.text_input("Instruct Embeddings", value="dbmdz/bert-base-italian-cased")
