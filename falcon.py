2e6260892328a6cf15e5f97bd94353ec5a523d00
Gerardo De Rosa <32306445+fauxl@users.noreply.github.com>
2025-01-11 01:24:51 +0100

    Update falcon.py

--- falcon.py
+++ falcon.py
@@ -78,7 +78,7 @@ def prepare_rag_llm(
         f"vector store/{vector_store_list}", instructor_embeddings, allow_dangerous_deserialization=True
     )
     llm = HuggingFaceHub(
-        repo_id = 'dbmdz/bert-base-italian-cased',
+        repo_id = "dbmdz/bert-base-italian-cased",
         model_kwargs={"temperature": temperature, "max_length": max_length},
         huggingfacehub_api_token=token
     )
